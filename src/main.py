a8a0266b757208d5d1d0a4cf8757f2fa72d7575b
IlyaBushmagin <ilyxa-bu@mail.ru>
2021-09-23 12:29:10 +0300

    Add src

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+f = pd.read_csv('ecg.csv')
+print(f)
+
+a = []
+n = len(f)
+for k in range(n):
+	l = 1.0 / (n - k)
+	sum = 0.0
+	for j in range(n - k):
+		delta = abs(f['ecg'][j + k] - f['ecg'][j])
+		sum = sum + delta
+	a.append(l * sum)
+
+plt.plot(f, linewidth = 0.5)
+plt.plot(a, linewidth = 0.5)
+plt.show()
